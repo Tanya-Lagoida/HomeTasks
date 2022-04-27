9d08bfa44c23343f906436b40b286e74507ce862
Tanya-Lagoida <tanusik-zar@yandex.ru>
2022-04-27 10:28:14 +0300

    HW8

--- src/p2-homeworks/h8/HW8.tsx
+++ src/p2-homeworks/h8/HW8.tsx
@@ -1,48 +1,61 @@
-import React, {useState} from 'react'
-import {homeWorkReducer} from './bll/homeWorkReducer'
-import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import React, { useState } from 'react';
+import { homeWorkReducer } from './bll/homeWorkReducer';
+import s from './style.module.css'
 
-// export type UserType =
-
-const initialPeople = [
-    {_id: 0, name: 'Кот', age: 3},
-    {_id: 1, name: 'Александр', age: 66},
-    {_id: 2, name: 'Коля', age: 16},
-    {_id: 3, name: 'Виктор', age: 44},
-    {_id: 4, name: 'Дмитрий', age: 40},
-    {_id: 5, name: 'Ирина', age: 55},
-]
-
-function HW8() {
-    const [people, setPeople] = useState<any>(initialPeople) // need to fix any
-
-    // need to fix any
-    const finalPeople = people.map((p: any) => (
-        <div key={p._id}>
-            some name, age
-        </div>
-    ))
-
-    const sortUp = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'}))
-
-    return (
-        <div>
-            <hr/>
-            homeworks 8
-
-            {/*should work (должно работать)*/}
-            {finalPeople}
-
-            <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
-            <div>sort down</div>
-            check 18
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativePeople/>*/}
-            <hr/>
-        </div>
-    )
+export type InitialStateType = {
+  _id: number
+  name: string
+  age: number
 }
 
-export default HW8
+const initialPeople = [
+  { _id: 0, name: 'Кот', age: 3 },
+  { _id: 1, name: 'Александр', age: 66 },
+  { _id: 2, name: 'Коля', age: 16 },
+  { _id: 3, name: 'Виктор', age: 44 },
+  { _id: 4, name: 'Дмитрий', age: 40 },
+  { _id: 5, name: 'Ирина', age: 55 }
+];
+
+function HW8() {
+  const [people, setPeople] = useState<InitialStateType[]>(initialPeople);
+
+  const finalPeople = people.map((p: InitialStateType) => (
+    <div key={p._id} className={s.people}>
+        <div className={s.name}>{p.name}</div>
+        <div className={s.age}>{p.age}</div>
+    </div>
+  ));
+
+  const sortUp = () => setPeople(homeWorkReducer(initialPeople, {
+    type: 'sort',
+    payload: 'up'
+  }));
+  const sortDown = () => setPeople(homeWorkReducer(initialPeople, {
+    type: 'sort',
+    payload: 'down'
+  }));
+  const check = () => setPeople(homeWorkReducer(initialPeople, {
+    type: 'check',
+    payload: 18
+  }));
+
+
+  return (
+    <div className={s.hw8}>
+      <hr/>
+      homeworks 8
+      {finalPeople}
+
+
+      <div className={s.allbutton}>
+      <button className={s.button} onClick={sortUp}>sort up</button>
+      <button className={s.button} onClick={sortDown}>sort down</button>
+      <button className={s.button} onClick={check}>check 18</button>
+      </div>
+
+    </div>
+  );
+}
+
+export default HW8;
--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,29 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': {
-            // need to fix
-            return state
-        }
-        case 'check': {
-            // need to fix
-            return state
-        }
-        default: return state
-    }
+import { InitialStateType } from '../HW8';
+
+type SortActionType = {
+  type: 'sort'
+  payload: 'up' | 'down'
 }
+type CheckActionType = {
+  type: 'check'
+  payload: 18
+}
+export type ActionType = SortActionType | CheckActionType
+
+export const homeWorkReducer = (state: InitialStateType[], action: ActionType): InitialStateType[] => {
+  switch (action.type) {
+    case 'sort': {
+        const newState = [...state].sort(function (a, b) {
+          if (a.name > b.name) return 1
+          else if (a.name < b.name) return -1
+          else return 0
+        })
+      return action.payload === 'up' ? newState : newState.reverse()
+    }
+    case 'check': {
+      return state.filter(u => u.age >= action.payload );
+    }
+    default:
+      return state;
+  }
+};
